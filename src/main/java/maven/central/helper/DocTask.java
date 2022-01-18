aee22dcf1d39c4aa6a58d5fae3f5e09e3e638b35
nbauma109 <nbauma109@github.com>
2022-01-18 20:58:11 +0100

    removed unused imports

--- src/main/java/maven/central/helper/DocTask.java
+++ src/main/java/maven/central/helper/DocTask.java
@@ -5,11 +5,8 @@ import org.jdesktop.swingx.decorator.HighlighterFactory;
 import org.oxbow.swingbits.list.CheckListRenderer;
 import org.oxbow.swingbits.table.filter.TableRowFilterSupport;
 
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
